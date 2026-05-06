51038d4796d448e9ec91b237b7f7846b1be83823
Alina <a72658695@gmail.com>
2026-05-06 13:06:41 +0300

    Step 7: mini task with DOM events

--- main.js
+++ main.js
@@ -37,3 +37,12 @@ button.addEventListener("click", () => {
     }
 });
 
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", () =>{
+    input.value = "";
+    output.textContent = "";
+    output.style.color = "";
+    input.focus();
+})
+
+
